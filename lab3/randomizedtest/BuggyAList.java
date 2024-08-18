9b6c8fd5578c18881acd01b4bc26f687dec8d014
mbh <i9222531@gmail.com>
2024-08-18 13:16:35 +0800

    done with lab3

--- lab3/randomizedtest/BuggyAList.java
+++ lab3/randomizedtest/BuggyAList.java
@@ -26,11 +26,9 @@ public class BuggyAList<Item> {
 
     /** Resizes the underlying array to the target capacity. */
     private void resize(int capacity) {
-        Item[] a = (Item[]) new Object[capacity];
-        for (int i = 0; i < size; i += 1) {
-            a[i] = items[i];
-        }
-        items = a;
+        Item[] new_array = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, new_array, 0, size);
+        items = new_array;
     }
 
     /** Inserts X into the back of the list. */
@@ -60,7 +58,7 @@ public class BuggyAList<Item> {
       * returns deleted item. */
     public Item removeLast() {
         if ((size < items.length / 4) && (size > 4)) {
-            resize(items.length / 4);
+            resize(items.length / 2);
         }
         Item x = getLast();
         items[size - 1] = null;
