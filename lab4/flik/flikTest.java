4fb640637ce237baabce6fb96705d04d7a315fdd
mbh <i9222531@gmail.com>
2024-08-20 16:34:47 +0800

    done with lab4

---
+++ lab4/flik/flikTest.java
@@ -0,0 +1,13 @@
+package flik;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class flikTest {
+
+    @Test
+    public void flikTest1() {
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+
+}
